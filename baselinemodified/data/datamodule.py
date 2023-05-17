117d8c266c1c46badfec9cd5fbd4c47628bb7f88
Hanqi Chen <hanqi.chen@polytechnique.edu>
2023-05-17 23:46:55 +0200

    add pseudolabel

--- baselinemodified/data/datamodule.py
+++ baselinemodified/data/datamodule.py
@@ -3,11 +3,41 @@ from torchvision.datasets import ImageFolder
 from hydra.utils import instantiate
 import torch
 
+import os
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import pandas as pd
+import torch
 
+class unlabelledDataset(Dataset):
+    def __init__(self, test_dataset_path, test_transform):
+        self.test_dataset_path = test_dataset_path
+        self.test_transform = test_transform
+        images_list = os.listdir(self.test_dataset_path)
+        # filter out non-image files
+        self.images_list = [image for image in images_list if image.endswith(".jpg")]
+        # set a non exist label
+        self.labels = [48] * len(self.images_list) 
+
+    def __getitem__(self, idx):
+        image_name = self.images_list[idx]
+        label = self.labels[idx]
+        image_path = os.path.join(self.test_dataset_path, image_name)
+        image = Image.open(image_path)
+        image = self.test_transform(image)
+        return image, label, idx
+
+    def __len__(self):
+        return len(self.images_list)
+    
+    def set_label(self, newlabel, idx):
+         self.labels[idx] = newlabel
+    
 class DataModule:
     def __init__(
         self,
         train_dataset_path,
+        unlabelled_dataset_path,
         train_transform,
         val_transform,
         aug_transform,
@@ -27,6 +57,9 @@ class DataModule:
         self.val_dataset.transform = val_transform
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.unlabelled_dataset_path = unlabelled_dataset_path
+        self.unlabelled_transform = train_transform
+        self.unlabelled_dataset = unlabelledDataset(self.unlabelled_dataset_path, self.unlabelled_transform)
 
     def data_augment(self, pic):
             pic_augment = self.aug_transform(pic)
@@ -47,3 +80,9 @@ class DataModule:
             shuffle=False,
             num_workers=self.num_workers,
         )
+    def unlabelled_dataloader(self):
+         return DataLoader(self.unlabelled_dataset,
+                        batch_size=self.batch_size,
+                        shuffle=False,
+                        num_workers=self.num_workers,
+                        )
--- baselinemodified/train.py
+++ baselinemodified/train.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 @hydra.main(config_path="configs", config_name="config", version_base=None)
 def train(cfg):
 
-    logger = wandb.init(project="challenge", name="new run 48 with DA 40 epochs")
+    logger = wandb.init(project="challenge", name="test")
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -19,6 +19,7 @@ def train(cfg):
 
     train_loader = datamodule.train_dataloader()
     val_loader = datamodule.val_dataloader()
+    unlabel_loader = datamodule.unlabelled_dataloader()
 
     for epoch in tqdm(range(cfg.epochs)):
         epoch_loss = 0
@@ -40,6 +41,7 @@ def train(cfg):
                 (preds.argmax(1) == labels).sum().detach().cpu().numpy()
             )
             num_samples += len(images)
+
         epoch_loss /= num_samples
         epoch_acc = epoch_num_correct / num_samples
         logger.log(
@@ -52,6 +54,35 @@ def train(cfg):
         epoch_loss = 0
         epoch_num_correct = 0
         num_samples = 0
+
+        #train on the pseudo labels
+        for i, batch in enumerate(unlabel_loader):
+            images, labels, idxs = batch
+            extracted_images = []
+            extracted_labels = []
+            for j in range(len(images)):
+                if labels[j]!=48:
+                    extracted_images.append(images[j].tolist())
+                    extracted_labels.append(labels[j].tolist())
+            extracted_images = torch.Tensor(extracted_images).to(device)
+            extracted_labels = torch.Tensor(extracted_labels).to(device)
+            if(len(extracted_labels)==0):
+                continue
+            preds = model(extracted_images)
+            loss = loss_fn(preds, extracted_labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        #setting pseudo labels
+        for i, batch in enumerate(unlabel_loader):
+            images, labels, idxs = batch
+            preds = torch.nn.functional.softmax(model(images),dim=-1)
+            for j in range(len(images)):
+                if labels[j]==48:
+                    pred = preds[j]
+                    if pred[pred.argmax()]>0.6:
+                        datamodule.unlabelled_dataset.set_label(pred.argmax(),idxs[j])
 
         for i, batch in enumerate(val_loader):
             images, labels = batch
