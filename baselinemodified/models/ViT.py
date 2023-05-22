bcbefaf1a9d89a037a899c11d7b88c70bf0f67fa
Hanqi Chen <hanqi.chen@polytechnique.edu>
2023-05-22 13:33:32 +0200

    epoch change

--- baselinemodified/models/ViT.py
+++ baselinemodified/models/ViT.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 class vitfinetune(nn.Module):
     def __init__(self, num_classes, frozen=False):
         super().__init__()
-        self.backbone = torchvision.models.vit_b_16(pretrained=True)
-        
+        self.backbone = torchvision.models.maxvit_t(pretrained=True)
+
         if frozen:
             for param in self.backbone.parameters():
                 param.requires_grad = False
