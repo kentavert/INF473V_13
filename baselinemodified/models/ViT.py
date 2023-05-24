fe244771f7bc2a45e2a3d2c30eab7322f068ac11
Hanqi Chen <hanqi.chen@polytechnique.edu>
2023-05-24 21:23:03 +0200

    config change

--- baselinemodified/models/ViT.py
+++ baselinemodified/models/ViT.py
@@ -6,11 +6,13 @@ class vitfinetune(nn.Module):
     def __init__(self, num_classes, frozen=False):
         super().__init__()
         self.backbone = torchvision.models.maxvit_t(pretrained=True)
-
+        self.backbone.classifier = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                                                nn.Flatten(),
+                                                nn.LayerNorm(512),)
         if frozen:
             for param in self.backbone.parameters():
                 param.requires_grad = False
-        self.classifier = nn.Linear(1000, num_classes)
+        self.classifier = nn.Linear(512, num_classes)
 
     def forward(self, x):
         x = self.backbone(x)
--- baselinemodified/models/resnet.py
+++ baselinemodified/models/resnet.py
@@ -6,11 +6,11 @@ class ResNetFinetune(nn.Module):
     def __init__(self, num_classes, frozen=False):
         super().__init__()
         self.backbone = torchvision.models.resnet50(pretrained=True)
-        #self.backbone.fc = nn.Identity()
+        self.backbone.fc = nn.Identity()
         if frozen:
             for param in self.backbone.parameters():
                 param.requires_grad = False
-        self.classifier = nn.Linear(1000, num_classes)
+        self.classifier = nn.Linear(2048, num_classes)
 
     def forward(self, x):
         x = self.backbone(x)
