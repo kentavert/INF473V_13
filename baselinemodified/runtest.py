70526e91fc88b87e292a5ff9c93c42e87669ee7b
Hanqi Chen <hanqi.chen@polytechnique.edu>
2023-05-29 01:30:56 +0200

    config change

--- baselinemodified/runtest.py
+++ baselinemodified/runtest.py
@@ -3,5 +3,6 @@ import torch
 cut = cutout.Cutout(1, 3)
 imgs = torch.rand(2,3,5,5)
 imgs = cut(imgs)
+torch.optim.SGD(lr=1e-2,momentum=0.9,weight_decay=1,nesterov=True)
 print(imgs.shape)
 print(imgs)
