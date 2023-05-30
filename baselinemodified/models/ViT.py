9135b1a50bf68f384a348a14d49addea70ed545d
Hanqi Chen <hanqi.chen@polytechnique.edu>
2023-05-30 14:59:38 +0200

    progress

--- baselinemodified/models/ViT.py
+++ baselinemodified/models/ViT.py
@@ -8,15 +8,19 @@ class vitfinetune(nn.Module):
         self.backbone = torchvision.models.maxvit_t(pretrained=True)
         self.backbone.classifier = nn.Sequential(nn.AdaptiveAvgPool2d(1),
                                                 nn.Flatten(),
-                                                nn.LayerNorm(512),)
+                                                nn.LayerNorm(512),
+                                                nn.Linear(512,512),
+                                                nn.Tanh(),
+                                                nn.Linear(512, num_classes, bias=False),)
         if frozen:
             for param in self.backbone.parameters():
                 param.requires_grad = False
-        self.classifier = nn.Sequential(nn.Linear(512, 1024),
+        
+        '''self.classifier = nn.Sequential(nn.Linear(512, 1024),
                                                 nn.ReLU(),
-                                                nn.Linear(1024, num_classes),)
+                                                nn.Linear(1024, num_classes),)'''
 
     def forward(self, x):
         x = self.backbone(x)
-        x = self.classifier(x)
+        #x = self.classifier(x)
         return x
